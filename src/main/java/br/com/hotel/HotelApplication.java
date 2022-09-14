191dee40125b67d51dd2303659217208e7391166
Matheus <matheus@pop-os.localdomain>
2022-09-14 17:32:22 -0300

    removendo relacionados a dependência de clienteWeb

--- src/main/java/br/com/hotel/HotelApplication.java
+++ src/main/java/br/com/hotel/HotelApplication.java
@@ -10,13 +10,6 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class HotelApplication {
-
-//	@Bean
-//	public WebClient webClient(WebClient.Builder builder) {
-//		return builder.baseUrl("htttp://localhost:8080")
-//				.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).build();
-//	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(HotelApplication.class, args);
 	}
